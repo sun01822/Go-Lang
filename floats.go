ccc160a726fa9a3af26505f0de8862bf411c5526
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-10-14 21:57:14 +0600

    float

---
+++ floats.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// float32 example
+	var myFloat32 float32 = 3.14
+	fmt.Printf("myFloat32 = %f, type = %T\n",myFloat32, myFloat32)
+
+
+	// float64 example
+	var myFloat64 float64 = 3.141592653589793238462643383279502
+    fmt.Printf("myFloat64 = %f, type = %T\n",myFloat64, myFloat64)
+
+
+}
