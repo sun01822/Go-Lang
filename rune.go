7e462fdfa2a590f5285fd60b9482979786e33ab1
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-10-15 10:02:28 +0600

    Rune

---
+++ rune.go
@@ -0,0 +1,21 @@
+// Simple Go program to illustrate
+// how to create a rune
+package main
+
+import (
+    "fmt"
+    "reflect"
+)
+
+func main() {
+
+    // Creating a rune
+    rune1 := 'B'
+    rune2 := 'g'
+    rune3 := '\a'
+    // Displaying rune and its type
+    fmt.Printf("Rune 1: %c; Unicode: %U; Type: %s", rune1, rune1, reflect.TypeOf(rune1))
+    fmt.Printf("\nRune 2: %c; Unicode: %U; Type: %s", rune2, rune2, reflect.TypeOf(rune2))
+    fmt.Printf("\nRune 3: Unicode: %U; Type: %s", rune3, reflect.TypeOf(rune3))
+
+}
