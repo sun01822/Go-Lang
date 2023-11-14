7d4b22d11a0906cf04b600f1ad716f31971b5c51
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-11-14 07:24:47 +0600

    How to use package

---
+++ galaxy/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"galaxy/milkway/solarsystem"
+)
+
+func main() {
+	fmt.Println("Starting...")
+	solarsystem.Hello()
+}
---
+++ galaxy/milkway/solarsystem/earth.go
@@ -0,0 +1,9 @@
+package solarsystem
+
+import (
+	"fmt"
+)
+
+func Hello(){
+	fmt.Println("Hello From earth!")
+}
