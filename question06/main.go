b84bfb555c1231b050c60fea2bf2ef4ff4dd7fd2
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-12-30 12:29:30 +0600

    Question 06 Vivasoft

---
+++ question06/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+type response2 struct {
+	Page   int      `json:"page_number"`
+	Fruits []string `json:"fruits-list"`
+}
+
+func main() {
+	jsonStr := `
+		{
+			"page-number": 1,
+			"fruits-list": ["apple", "peach"]
+		}`
+
+	res := response2{}
+	json.Unmarshal([]byte(jsonStr), &res)
+	fmt.Println(res)
+}
