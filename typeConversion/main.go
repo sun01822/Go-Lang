fc4d6f941cd586c4969bd56970946099974e3b18
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2024-01-08 19:29:20 +0600

    Type Converstion and Data fomating

---
+++ typeConversion/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var iNum int = 10
+	var fNum float32 = float32(iNum)
+	fmt.Printf("value = %v | type - %T\n", fNum, fNum)
+	var fNum2 = 5.65
+	iNum1 := int(fNum2)
+	fmt.Printf("value = %v | type - %T\n", iNum1, iNum1)
+
+	var num1 float64 = 10.5
+	var num2 int = 5
+	result := float64(num1) + float64(num2)
+	fmt.Printf("value = %v | type - %T\n", result, result)
+
+
+	// formating float
+	var d1 int = 10
+	fmt.Printf("in binary=%b\n", d1)
+	fmt.Printf("in decimal=%d\n", d1)
+	fmt.Printf("in hex=%x\n", d1)
+	fmt.Printf("in octal=%o\n", d1)
+
+}
